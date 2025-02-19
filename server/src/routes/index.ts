3ebed981bcf2560950ac73c77d96323d15de6c6c
Jeremy Osner <anacreon@gmail.com>
2025-02-19 09:26:00 -0500

    typescript errors

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,16 +2,16 @@ import type { Request, Response } from 'express';
 import express from 'express';
 const router = express.Router();
 
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//import path from 'node:path';
+//import { fileURLToPath } from 'node:url';
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
 import apiRoutes from './api/index.js';
 
 router.use('/api', apiRoutes);
 
 // serve up react front-end in production
-router.use((req: Request, res: Response) => {
+router.use((req: Request, _res: Response) => {
   console.log('request for unknown path', req.url)
   //res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
