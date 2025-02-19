6b8dea19c6862395b44e2173cc2a12529bc00255
Jeremy Osner <anacreon@gmail.com>
2025-02-19 09:33:22 -0500

    move express.static call before express.urlencoded call

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,12 +12,12 @@ import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.use(express.static('../../client/dist'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-app.use(express.static('../../client/dist'));
 
 
 app.use(routes);
