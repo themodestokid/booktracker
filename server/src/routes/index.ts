c1b51cb8ec83876a3dbcd19e0aa4a07082453f27
Jeremy Osner <anacreon@gmail.com>
2025-02-19 09:22:37 -0500

    remove incorrect default handing for server

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -11,8 +11,9 @@ import apiRoutes from './api/index.js';
 router.use('/api', apiRoutes);
 
 // serve up react front-end in production
-router.use((_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+router.use((req: Request, res: Response) => {
+  console.log('request for unknown path', req.url)
+  //res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
 
 export default router;
