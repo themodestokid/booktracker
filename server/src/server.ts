989e1fc0c14339e64198b506b7d0be67bec850fe
Jeremy Osner <anacreon@gmail.com>
2025-02-19 09:54:11 -0500

    fix relative path to client in express.static call

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.static('../../client/dist'));
+app.use(express.static('client/dist'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
